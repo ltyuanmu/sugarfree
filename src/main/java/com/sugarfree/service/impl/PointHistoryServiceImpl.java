aae5f52ee5127c6f40a0d49e438ed84f61054b8f
liangteng <teng.liang@newtouch.cn>
2016-11-26 15:24:55 +0800

    update

--- src/main/java/com/sugarfree/service/impl/PointHistoryServiceImpl.java
+++ src/main/java/com/sugarfree/service/impl/PointHistoryServiceImpl.java
@@ -4,10 +4,12 @@ import com.sugarfree.dao.mapper.TPointHistoryMapper;
 import com.sugarfree.dao.model.TPointHistory;
 import com.sugarfree.service.PointHistoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by Administrator on 2016/11/24.
  */
+@Service
 public class PointHistoryServiceImpl implements PointHistoryService {
     @Autowired
     private TPointHistoryMapper tPointHistoryMapper;
